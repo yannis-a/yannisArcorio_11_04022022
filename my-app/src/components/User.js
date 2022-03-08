c5ef90f49dcbadab11f24d4bb7553250bc70072e
Yannis <arcorio.y@gmail.com>
2022-03-08 22:59:08 +0100

    user component

--- my-app/src/components/User.js
+++ my-app/src/components/User.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const User = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const User = ({ host }) => {
+  const firstName = host.name.split(" ")[0];
+  const lastName = host.name.split(" ")[1];
+  return (
+    <div className="user">
+      <div className="name">
+        <p>{firstName}</p>
+        <p>{lastName}</p>
+      </div>
+      <div className="picture">
+        <img src={host.picture} alt="profil" />
+      </div>
+    </div>
+  );
 };
 
 export default User;
