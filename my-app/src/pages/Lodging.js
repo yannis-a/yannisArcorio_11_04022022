82383edaa83aa5cb146e8e44d69b7405847ed85a
Yannis <arcorio.y@gmail.com>
2022-04-05 21:10:13 +0200

    fix bug

--- my-app/src/pages/Lodging.js
+++ my-app/src/pages/Lodging.js
@@ -6,31 +6,35 @@ import Carousel from "../components/Carousel";
 import Rating from "../components/Rating";
 import { useParams } from "react-router-dom";
 import data from "../data.json";
+import NotFound from "./NotFound";
 
 const Kasa = () => {
   const params = useParams();
   const lodgingData = data.find((e) => e.id === params.id);
-  return (
-    <div className="lodging">
-      <Carousel pictures={lodgingData.pictures} />
-      <section className="informations">
-        <div>
-          <h1>{lodgingData.title}</h1>
-          <h4>{lodgingData.location}</h4>
-          <Tags tags={lodgingData.tags} />
-        </div>
-        <div>
-          <User host={lodgingData.host} />
-          <Rating rating={lodgingData.rating} />
-        </div>
-      </section>
-
-      <section className="descriptions">
-        <CardText title="Description" text={[lodgingData.description]} />
-        <CardText title="Équipements" text={lodgingData.equipments} />
-      </section>
-    </div>
-  );
+  if (lodgingData !== undefined) {
+    return (
+      <div className="lodging">
+        <Carousel pictures={lodgingData.pictures} />
+        <section className="informations">
+          <div>
+            <h1>{lodgingData.title}</h1>
+            <h4>{lodgingData.location}</h4>
+            <Tags tags={lodgingData.tags} />
+          </div>
+          <div>
+            <User host={lodgingData.host} />
+            <Rating rating={lodgingData.rating} />
+          </div>
+        </section>
+        <section className="descriptions">
+          <CardText title="Description" text={[lodgingData.description]} />
+          <CardText title="Équipements" text={lodgingData.equipments} />
+        </section>
+      </div>
+    );
+  }else{
+    return <NotFound />
+  }
 };
 
 export default Kasa;
